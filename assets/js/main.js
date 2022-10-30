93e419381289163b3898dedaf50e5dd147e066bd
joaoaalem <joao.aalem@gmail.com>
2022-10-30 00:08:17 -0300

    ajuste gerais e adição de novas paginas

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,15 +7,96 @@ function validaFormContato(){
         email.className += " border border-warning";
         alert("Informe os campos obrigatórios");
         return false;
-    }else if(nome.value == ""){
+    }
+
+    if(nome.value == ""){
         nome.className += " border border-warning";
         alert("Informe os campos obrigatórios");
         return false;
-    }else if(mensagem.value == ""){
+    }
+
+    if(mensagem.value == ""){
         mensagem.className += " border border-warning";
         alert("Informe os campos obrigatórios");
         return false;
     }
+}
+
+function validaCadastro(){
+    let nome = document.forms["formCadastro"]["fNome"];
+    let sobrenome = document.forms["formCadastro"]["fSobrenome"];
+    let email = document.forms["formCadastro"]["fEmail"];
+    let confirmaEmail = document.forms["formCadastro"]["fConfirmaEmail"];
+    let senha = document.forms["formCadastro"]["fSenha"];
+    let confirmaSenha = document.forms["formCadastro"]["fConfirmaSenha"];
+
+    if(nome.value == ""){
+        nome.className += " border border-warning";
+        alert("Informe os campos obrigatórios");
+        return false;
+    }
+
+    if(sobrenome.value == ""){
+        sobrenome.className += " border border-warning";
+        alert("Informe os campos obrigatórios");
+        return false;
+    }
+
+    if(email.value == ""){
+        email.className += " border border-warning";
+        alert("Informe os campos obrigatórios");
+        return false;
+    }
+
+    if(confirmaEmail.value == ""){
+        confirmaEmail.className += " border border-warning";
+        alert("Informe os campos obrigatórios");
+        return false;
+    }
+
+    if(email.value != confirmaEmail.value){
+        email.className += " border border-warning";
+        confirmaEmail.className += " border border-warning";
+        alert("Email incorreto com o campo confirma Email");
+        return false;
+    }
+
+    if(senha.value == ""){
+        senha.className += " border border-warning";
+        alert("Informe os campos obrigatórios");
+        return false;
+    }
+
+    if(confirmaSenha.value == ""){
+        senha.className += " border border-warning";
+        alert("Informe os campos obrigatórios");
+        return false;
+    }
+
+    if(senha.value != confirmaSenha.value){
+        senha.className += " border border-warning";
+        confirmaSenha.className += " border border-warning";
+        alert("Senha incorreto com o campo confirma senha");
+        return false;
+    }
+}
+
+function validaLogin(){
+    let email = document.forms["formLogin"]["fEmail"];
+    let senha = document.forms["formLogin"]["fSenha"];
+
+    if(email.value == ""){
+        email.className += " border border-warning";
+        alert("Informe os campos obrigatórios");
+        return false;
+    }
+
+    if(senha.value == ""){
+        senha.className += " border border-warning";
+        alert("Informe os campos obrigatórios");
+        return false;
+    }
+
 }
 
 function carregarMais(endereco, elemento){
